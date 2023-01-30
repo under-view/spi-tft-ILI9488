a6735913fc46b9223a35366e6b78d6630c613b1f
Vincent Davis Jr <vince@underview.tech>
2023-01-30 14:37:44 -0600

    ili9488: init SPI device setup
    Signed-off-by: Vincent Davis Jr <vince@underview.tech>

--- ili9488.c
+++ ili9488.c
@@ -4,8 +4,46 @@
 #include <linux/module.h>
 #include <linux/spi/spi.h>
 
+#define SPI_BUS_NUM 0
+#define SPI_SLAVE_SELECT_NUM 0
+
+static struct spi_board_info ili9488_device_info = {
+	.modalias      = "underview-tft-spi-ili9488-driver", /* Driver Identifier */
+	.max_speed_hz  = 32000000,                           /* speed your device (slave) can handle */
+	.bus_num       = SPI_BUS_NUM,                        /* SPI: 0 */
+	.chip_select   = SPI_SLAVE_SELECT_NUM,               /* CS/SS: 0 */
+	.mode          = SPI_MODE_0,                         /* SPI MODE 0 CPOL (Clock Polarity) -> low & CPHA (Clock Phase) -> low */
+	.platform_data = NULL,                               /* No spi_driver specific config */
+};
+
+
 static int ili9488_probe(struct spi_device *spidev)
 {
+	struct spi_master *master = NULL;
+	struct spi_device *slave = NULL;
+	struct device *device = NULL;
+	int ret;
+
+	device = &spidev->dev;
+
+	master = devm_spi_alloc_slave(device, sizeof(*master));
+	if (!master) {
+		dev_err(device, "devm_spi_alloc_slave: master allocation failed\n");
+		return -ENOMEM;
+	}
+
+	slave = spi_new_device(master, &ili9488_device_info);
+	if (!slave) {
+		dev_err(device, "spi_new_device: failed to create slave");
+		return -ENODEV;
+	}
+
+	ret = spi_setup(slave);
+	if (ret < 0) {
+		dev_err(device, "spi_setup: failed to setup slave");
+		return ret;
+	}
+
 	return 0;
 }
 
