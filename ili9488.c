954385af62f8d30a6d685d45489b31bad9decf61
Vincent Davis Jr <vince@underview.tech>
2023-02-02 13:29:10 -0600

    ili9488: add DT YAML & update of_match_table
    Make the driver compatible with the ili9488 dtb.
    So, devices of said type are bound to driver.
    
    Can load the device tree in GRUB with the
    devicetree command.
    
    Signed-off-by: Vincent Davis Jr <vince@underview.tech>

--- ili9488.c
+++ ili9488.c
@@ -1,69 +1,65 @@
 // SPDX-License-Identifier: GPL-2.0
 
-#include <linux/init.h>
+#include <linux/backlight.h>
+#include <linux/delay.h>
+#include <linux/gpio/consumer.h>
 #include <linux/module.h>
+#include <linux/property.h>
 #include <linux/spi/spi.h>
 
-#define SPI_BUS_NUM 0
-#define SPI_SLAVE_SELECT_NUM 0
+#include <video/mipi_display.h>
 
-static struct spi_board_info ili9488_device_info = {
-	.modalias      = "underview-tft-spi-ili9488-driver", /* Driver Identifier */
-	.max_speed_hz  = 32000000,                           /* speed your device (slave) can handle */
-	.bus_num       = SPI_BUS_NUM,                        /* SPI: 0 */
-	.chip_select   = SPI_SLAVE_SELECT_NUM,               /* CS/SS: 0 */
-	.mode          = SPI_MODE_0,                         /* SPI MODE 0 CPOL (Clock Polarity) -> low & CPHA (Clock Phase) -> low */
-	.platform_data = NULL,                               /* No spi_driver specific config */
-};
-
+#include <drm/drm_atomic_helper.h>
+#include <drm/drm_drv.h>
+#include <drm/drm_fb_helper.h>
+#include <drm/drm_gem_atomic_helper.h>
+#include <drm/drm_managed.h>
+#include <drm/drm_mipi_dbi.h>
+#include <drm/drm_modeset_helper.h>
 
 static int ili9488_probe(struct spi_device *spidev)
 {
-	struct spi_master *master = NULL;
-	struct spi_device *slave = NULL;
 	struct device *device = NULL;
-	int ret;
 
 	device = &spidev->dev;
 
-	master = devm_spi_alloc_slave(device, sizeof(*master));
-	if (!master) {
-		dev_err(device, "devm_spi_alloc_slave: master allocation failed\n");
-		return -ENOMEM;
-	}
-
-	slave = spi_new_device(master, &ili9488_device_info);
-	if (!slave) {
-		dev_err(device, "spi_new_device: failed to create slave");
-		return -ENODEV;
-	}
-
-	ret = spi_setup(slave);
-	if (ret < 0) {
-		dev_err(device, "spi_setup: failed to setup slave");
-		return ret;
-	}
-
 	return 0;
 }
-
 
 static void ili9488_remove(struct spi_device *spidev)
 {
 	return;
 }
 
+/* Device Tree Node to be compatible with */
+static const struct of_device_id ili9488_of_match[] = {
+	{ .compatible = "garosa,garosanvkr25fawd" },
+	{},
+};
+
+MODULE_DEVICE_TABLE(of, ili9488_of_match);
+
+static const struct spi_device_id ili9488_spi_id[] = {
+	{ "ili9488", 0 },
+	{ }
+};
+
+MODULE_DEVICE_TABLE(spi, ili9488_spi_id);
+
 /* Driver declaration */
-static struct spi_driver ili9488_driver = {
+static struct spi_driver ili9488_spi_driver = {
 	.driver = {
 		.name = "ili9488",
 		.owner = THIS_MODULE,
+		.of_match_table = ili9488_of_match,
 	},
+	.id_table = ili9488_spi_id,
 	.probe = ili9488_probe,
 	.remove = ili9488_remove,
 };
 
-module_spi_driver(ili9488_driver);
+module_spi_driver(ili9488_spi_driver);
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Underview");
+MODULE_DESCRIPTION("Ilitek ILI9488 DRM driver");
