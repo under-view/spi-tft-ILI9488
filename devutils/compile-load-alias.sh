a9d0168c5c72d50b8702a854b2b2be7bdd0e4bd8
Vincent Davis Jr <vince@underview.tech>
2023-03-01 13:55:39 -0600

    compile-load-alias: fix DEVUTILS_DIR bug
    Problem:
    	* Absolute path to files directory
    	  is incorrect.
    Fixes:
    	* Use realpath with dirname is acquire
    	  correct absolute path.
    
    Additions:
    	* script is an executable
    	* Changes the variable name DEVUTILS_DIR
    	  to ILI9488_DEVUTILS_DIR. That way if
    	  a similar script with similar variable
              is sourced there's less of a chance
              of being overridden.
    	* adds mount_enable_acpi_configfs alias
    	  for easier loading of acpi-configfs
    	  kernel module
    
    Signed-off-by: Vincent Davis Jr <vince@underview.tech>

--- devutils/compile-load-alias.sh
+++ devutils/compile-load-alias.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-DEVUTILS_DIR=$(dirname "${BASH_SOURCE[0]}")
+ILI9488_DEVUTILS_DIR=$(dirname $(realpath "${BASH_SOURCE[0]}"))
 
-alias compile_ili9488_aml="make -C ${DEVUTILS_DIR}/../ aml"
-alias load_ili9488_aml="mkdir -p /sys/kernel/config/acpi/table/ili9488; cat ${DEVUTILS_DIR}/../ili9488.aml > /sys/kernel/config/acpi/table/ili9488/aml"
-alias unload_ili9488_aml="${DEVUTILS_DIR}/unload-acpi-table /sys/kernel/config/acpi/table/ili9488"
+alias mount_enable_acpi_configfs="mount -t configfs none /sys/kernel/config 2>/dev/null && modprobe acpi-configfs"
+alias compile_ili9488_aml="make -C ${ILI9488_DEVUTILS_DIR}/../ aml"
+alias load_ili9488_aml="mkdir -p /sys/kernel/config/acpi/table/ili9488; cat ${ILI9488_DEVUTILS_DIR}/../ili9488.aml > /sys/kernel/config/acpi/table/ili9488/aml"
+alias unload_ili9488_aml="${ILI9488_DEVUTILS_DIR}/unload-acpi-table /sys/kernel/config/acpi/table/ili9488"
